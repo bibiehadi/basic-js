2c10d388c4b83d181edefd775255a965c876ba11
Bibie Hadi <bibie.hadi22@gmail.com>
2023-02-17 00:19:13 +0700

    done 00028 & 00030

--- 00028.js
+++ 00028.js
@@ -1,11 +1,27 @@
 // Check AB
 
-// Diberikan sebuah function checkAB(str) yang menerima satu parameter berupa string. 
-// function tersebut mengembalikan nilai true jika di dalam string tersebut terdapat karakter a dan b yang memiliki jarak 3 karakter lain minimal satu kali. 
+// Diberikan sebuah function checkAB(str) yang menerima satu parameter berupa string.
+// function tersebut mengembalikan nilai true jika di dalam string tersebut terdapat karakter a dan b yang memiliki jarak 3 karakter lain minimal satu kali.
 // Jika tidak ditemukan sama sekali, kembalikan nilai false.
 
 function checkAB(num) {
   // you can only write your code here!
+  const arr = num.split('');
+  let bIndex = null;
+  let aIndex = null;
+  for (let i = 0; i < arr.length; i++) {
+    if (bIndex - aIndex >= 3) {
+      return true;
+    }
+    if (arr[i] === 'a') {
+      aIndex = i;
+    }
+    if (arr[i] === 'b') {
+      bIndex = i;
+    }
+    // console.log(aIndex + ' ' + bIndex);
+  }
+  return false;
 }
 
 // TEST CASES
--- 00033.js
+++ 00033.js
@@ -17,22 +17,44 @@
 
 function changeVocals(str) {
   //code di sini
-  return reverseWord(str);
+  const vokal = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
+  const arr = str.split('');
+  for (let i = 0; i < vokal.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === vokal[i]) {
+        arr[j] = String.fromCharCode(arr[j].charCodeAt() + 1);
+      }
+    }
+  }
+  return reverseWord(arr.join(''));
 }
 
 function reverseWord(str) {
   //code di sini
-  return setLowerUpperCase(str);
+  const arr = str.split('');
+  const reverse = [];
+  for (let i = arr.length + 1; i >= 0; i--) {
+    reverse.push(arr[i]);
+  }
+  return setLowerUpperCase(reverse.join(''));
 }
 
 function setLowerUpperCase(str) {
   //code di sini
-  return removeSpaces(str);
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i].toUpperCase()) {
+      arr[i] = arr[i].toUpperCase();
+    } else if (arr[i] !== arr[i].toLowerCase()) {
+      arr[i] = arr[i].toLowerCase();
+    }
+  }
+  return removeSpaces(arr.join(''));
 }
 
 function removeSpaces(str) {
-  //code di sini
-  return str.trim(' ');
+  const arr = str.split(' ');
+  return arr.join('');
 }
 
 function passwordGenerator(name) {
