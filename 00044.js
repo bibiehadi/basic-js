0a0861d93fb0ab02281962c6927b04a596e0c0fe
Bibie Hadi <bibie.hadi22@gmail.com>
2023-02-17 10:33:23 +0700

    done 00044

--- 00044.js
+++ 00044.js
@@ -3,28 +3,45 @@
 //     * nama player
 //     * posisi player
 //   - lengthOfTrack bertipe number dimana ini mewakilkan panjang lintasan
-  
+
 //   Asumsi posisi tidak pernah 0 dan angka negatif.
 
 //   Contoh hasil output bisa dilihat pada test case/driver code
 
-
 function drawTrack(players, lengthOfTrack) {
-    
+  const track = [];
+  for (let i = 0; i < lengthOfTrack; i++) {
+    track[i] = '';
+    players.forEach((player) => {
+      if (player[1] === i + 1) {
+        track[i] = player[0];
+      }
+    });
+  }
+  return track;
 }
-  
-console.log(drawTrack([
-    ['Lee Minho', 5],
-    ['Song Joong Ki', 7]
-], 15));
+
+console.log(
+  drawTrack(
+    [
+      ['Lee Minho', 5],
+      ['Song Joong Ki', 7],
+    ],
+    15
+  )
+);
 
 // [ '', '', '', '', 'Lee Minho', '', 'Song Joong Ki', '', '', '', '', '', '', '', '' ]
 
-console.log(drawTrack([
-    ['Xavier Fox', 2],
-    ['Young Fox', 3],
-    ['Zen Fox', 4]
-], 9));
+console.log(
+  drawTrack(
+    [
+      ['Xavier Fox', 2],
+      ['Young Fox', 3],
+      ['Zen Fox', 4],
+    ],
+    9
+  )
+);
 
 // [ [ '', 'Xavier Fox', 'Young Fox', 'Zen Fox', '', '', '', '', '' ]
-  
